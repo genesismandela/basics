aad33eaeaedb14ed321119f1a245032e93a9288e
genesismandela <mandelasamboy@gmail.com>
2020-07-08 14:57:25 +0100

    Changes for Portfolio
    Burger Navigation for mobile device working properly in all pages and changed few style properties in all pages.

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
+const menuBtn = document.getElementById('nav-burger');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+});
+
 document.getElementById('nav-burger').
 addEventListener('click', function() {
-  var burgerDOM = document.getElementById('nav-burger');
-  burgerDOM.style.display = 'none';
+  var navShowHide = document.getElementById('nav-list');
+  if (navShowHide.style.visibility === 'visible') {
+    navShowHide.style.visibility = 'hidden';
+  } else {
+    navShowHide.style.visibility = 'visible';
+  }
 
-  document.getElementById('title-container').style.display = 'none';
-  document.getElementById('social-media-container').style.display = 'none';
-
-  document.getElementById('nav-list').style.visibility = 'visible';
+  var elementsShowHide = [
+    [document.getElementById('main-title')], 
+    [document.getElementById('social-media-container')], 
+    [document.querySelector('.btn')]];
+  if (navShowHide.style.visibility === 'visible') {
+    elementsShowHide.style.visibility = 'hidden';
+  } else {
+    elementsShowHide.style.visibility = 'visible';
+  }
 });
